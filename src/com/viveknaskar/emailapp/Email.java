e45219f784a3e76331b22f34ea9dbd62814b1f03
Vivek Naskar <viveknaskar2912@gmail.com>
2020-07-07 13:38:41 +0530

    Created the POJO class required for the email app

--- src/com/viveknaskar/emailapp/Email.java
+++ src/com/viveknaskar/emailapp/Email.java
@@ -5,5 +5,11 @@ package com.viveknaskar.emailapp;
  */
 public class Email {
 
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
 
 }
