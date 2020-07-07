090bc311b3288db6fc31ecff66c5d106a603c0cb
Vivek Naskar <viveknaskar2912@gmail.com>
2020-07-07 13:30:47 +0530

    Created an outline for the email application

---
+++ src/com/viveknaskar/emailapp/Email.java
@@ -0,0 +1,9 @@
+package com.viveknaskar.emailapp;
+
+/**
+ * The domain class containing the object
+ */
+public class Email {
+
+
+}
---
+++ src/com/viveknaskar/emailapp/EmailApp.java
@@ -0,0 +1,12 @@
+package com.viveknaskar.emailapp;
+
+/**
+ * The main class which bootstraps the application
+ */
+public class EmailApp {
+
+    public static void main(String[] args) {
+
+    }
+
+}
